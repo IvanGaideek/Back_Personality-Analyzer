2fee646c5c198af2a9a38911a1708f2b9c91516e
вашÐIvanGaideek <workgaideek37@gmail.com>
2024-12-12 22:54:20 +0300

    add OrJson for fast works data

--- application/main.py
+++ application/main.py
@@ -1,40 +1,22 @@
 import logging
-from contextlib import asynccontextmanager
 
 import uvicorn
-from fastapi import FastAPI
 
 from core.config import settings
 
 from application.api import router as api_router
-from core.models import db_helper, Base
 
 from create_fastapi_app import create_app
 
 
 logging.basicConfig(
-    # level=logging.INFO
+    level=logging.INFO,
     format=settings.logging.log_format,
 )
 
-# main_app = create_app(
-#     create_custom_static_urls=True,
-# )
-
-
-###
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # startup
-    yield
-    # shutdown
-    await db_helper.dispose()
-
-
-main_app = FastAPI(
-    lifespan=lifespan
+main_app = create_app(
+    create_custom_static_urls=True,
 )
-###
 
 main_app.include_router(
     api_router,
