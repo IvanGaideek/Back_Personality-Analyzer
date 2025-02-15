35a76732938d81d75af6b296cedeb046469a288d
Maks <85566109+MaksonInTouch@users.noreply.github.com>
2025-02-15 23:15:35 +0300

    Update user.py

--- application/core/models/user.py
+++ application/core/models/user.py
@@ -1,16 +1,23 @@
-from sqlalchemy import UniqueConstraint
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
-
-from .base import Base
-from .mixins.int_id_pk import IntIdPkMixin
+from sqlalchemy import text
+from sqlalchemy.orm import Mapped, mapped_column
+from app.database import Base, str_uniq, int_pk
 
 
-class User(IntIdPkMixin, Base):
-    username: Mapped[str] = mapped_column(unique=True)
-    foo: Mapped[int]
-    bar: Mapped[int]
+class User(Base):
+    id: Mapped[int_pk]
+    phone_number: Mapped[str_uniq]
+    first_name: Mapped[str]
+    last_name: Mapped[str]
+    email: Mapped[str_uniq]
+    password: Mapped[str]
 
-    __table_args__ = (
-        UniqueConstraint("foo", "bar"),
-    )
+    is_user: Mapped[bool] = mapped_column(default=True, server_default=text('true'), nullable=False)
+    is_student: Mapped[bool] = mapped_column(default=False, server_default=text('false'), nullable=False)
+    is_teacher: Mapped[bool] = mapped_column(default=False, server_default=text('false'), nullable=False)
+    is_admin: Mapped[bool] = mapped_column(default=False, server_default=text('false'), nullable=False)
+    is_super_admin: Mapped[bool] = mapped_column(default=False, server_default=text('false'), nullable=False)
+
+    extend_existing = True
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(id={self.id})"
