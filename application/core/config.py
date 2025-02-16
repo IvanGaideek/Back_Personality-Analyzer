8697a4ed5c7d84079134a39a64106c95e9a598dd
Maks <85566109+MaksonInTouch@users.noreply.github.com>
2025-02-16 16:26:12 +0300

    Update config.py

--- application/core/config.py
+++ application/core/config.py
@@ -6,6 +6,7 @@ from pydantic_settings import (
     BaseSettings,
     SettingsConfigDict,
 )
+import os
 
 
 LOG_DEFAULT_FORMAT = "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
@@ -73,5 +74,19 @@ class Settings(BaseSettings):
     api: ApiPrefix = ApiPrefix()
     db: DatabaseConfig = DatabaseConfig()
 
+ DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env")
+    )
+
 
 settings = Settings()
+
+def get_auth_data():
+    return {"secret_key": settings.SECRET_KEY, "algorithm": settings.ALGORITHM}
