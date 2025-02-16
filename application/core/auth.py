7c237838f8220278f3bf7393947fb13abc72383c
Maks <85566109+MaksonInTouch@users.noreply.github.com>
2025-02-16 21:18:42 +0300

    Update auth.py

--- application/core/auth.py
+++ application/core/auth.py
@@ -2,8 +2,8 @@ from passlib.context import CryptContext
 from pydantic import EmailStr
 from jose import jwt
 from datetime import datetime, timedelta, timezone
-from app.config import get_auth_data
-from app.users.dao import UsersDAO
+from core.config.Settings.db import url
+from core.models.dao.py import UsersDAO
 
 
 def create_access_token(data: dict) -> str:
