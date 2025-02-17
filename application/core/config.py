c94329d31d1c7a7c808ce43738f851af06a45ffe
Maks <85566109+MaksonInTouch@users.noreply.github.com>
2025-02-18 00:15:25 +0300

    Update config.py

--- application/core/config.py
+++ application/core/config.py
@@ -74,14 +74,14 @@ class Settings(BaseSettings):
     api: ApiPrefix = ApiPrefix()
     db: DatabaseConfig = DatabaseConfig()
 
- DB_HOST: str
+    DB_HOST: str
     DB_PORT: int
     DB_NAME: str
     DB_USER: str
     DB_PASSWORD: str
     SECRET_KEY: str
     ALGORITHM: str
-    model_config = SettingsConfigDict(
+model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env")
     )
 
